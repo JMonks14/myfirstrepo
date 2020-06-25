53497bb2e53425937a347d0d07d493337aeb7dda
James Monks <James.Monks@academytrainee.com>
2020-06-25 11:14:51 +0100

    added helloworld java program

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package helloworld;
+
+public class HelloWorld {
+	
+	public static void printMessage(String message) {
+		
+		System.out.println(message);
+	}
+	
+	public static void main(String[] args) {
+		printMessage("Hello World");
+	}
+
+}
